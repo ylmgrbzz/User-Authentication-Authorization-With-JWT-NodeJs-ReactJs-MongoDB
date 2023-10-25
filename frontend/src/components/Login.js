7f051b1444544aad8dc9a7db9f57128c94edd049
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-25 19:02:35 +0300

    login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,80 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>
-        ylm
-    </div>
-  )
-}
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+      });
+    
+      const navigate = useNavigate();
+    
+      const sendRequest = async () => {
+        const response = await axios
+          .post("http://localhost:5000/api/login", {
+            email: inputs.email,
+            password: inputs.password,
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        const data = await response.data;
+        return data;
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = await sendRequest();
+        if (data.success) {
+          navigate("/login");
+        }
+      };
+    
+      const handleChange = (e) => {
+        setInputs((state) => ({ ...state, [e.target.name]: e.target.value }));
+        console.log(inputs);
+      };
+    
+      return (
+        <div>
+          <Box
+            marginLeft="auto"
+            marginRight="auto"
+            width={300}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            onSubmit={handleSubmit}
+            component="form"
+          >
+            <Typography variant="h3">Login</Typography>
+            <TextField
+              value={inputs.email}
+              type="email"
+              margin="normal"
+              name="email"
+              variant="outlined"
+              placeholder="email"
+              onChange={handleChange}
+            />
+            <TextField
+              value={inputs.password}
+              type="password"
+              onChange={handleChange}
+              margin="normal"
+              variant="outlined"
+              name="password"
+              placeholder="password"
+            />
+            <Button variant="contained" type="submit">
+              Login
+            </Button>
+          </Box>
+        </div>
+      );
+    };
 
 export default Login
