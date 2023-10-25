d9fab605aa7f18604f04ed4a67be98708c19092f
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-25 15:09:54 +0300

    user modal

---
+++ model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
