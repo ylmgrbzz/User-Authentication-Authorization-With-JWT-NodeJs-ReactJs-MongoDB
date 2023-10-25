e6b1dbf60ea644700152a357961243b28e8117a1
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-25 18:59:03 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/user-routes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
+app.use(cors());
 
 const connectToDatabase = async () => {
   try {
