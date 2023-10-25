99a5d9d32a886508c3ac223dee3be8786ce4f004
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-25 16:27:58 +0300

    jsonwebtoken

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,8 @@
-const User = require("../modal/User");
+const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-export const signup = async (req, res, next) => {
+const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email: email });
@@ -25,3 +26,34 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+    const isPasswordValid = await bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+    const token = jwt.sign({ id: user._id }, "secret_key", {
+      expiresIn: "1hr",
+    });
+    res.status(200).json({
+      message: "Login successful",
+      user: user,
+      token: token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signup = signup;
+exports.login = login;
